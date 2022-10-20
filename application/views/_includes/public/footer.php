b132360f04cc89ff666de9c2a2f90bf65a7acb0e
lalaxizuna <farelmaestro0@gmail.com>
2022-10-20 23:25:03 +0700

    first commit

---
+++ application/views/_includes/public/footer.php
@@ -0,0 +1,33 @@
+</div>
+<footer class="footer footer-transparent d-print-none">
+    <div class="container">
+        <div class="row text-center align-items-center flex-row-reverse">
+            <div class="col-lg-auto ml-lg-auto">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item"><a href="yokosoizuna.lovestoblog.com" target="_blank" class="link-secondary">Klik Untuk Melihat Orang Tampan</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                   
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+<!-- Libs JS -->
+<script src="<?= base_url(); ?>assets/dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jquery/dist/jquery.slim.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/apexcharts/dist/apexcharts.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jqvmap/dist/jquery.vmap.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<!-- Tabler Core -->
+<script src="<?= base_url(); ?>assets/dist/js/tabler.min.js"></script>
+<script>
+    document.body.style.display = "block"
+</script>
+</body>
+
+</html>
